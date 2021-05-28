68edc8b6f2311a471f961e04cfab4238087a29f1
古里 尚英 <32025@ojs.ac.jp>
2021-05-28 16:00:37 +0900

    P79 問題2.1

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace Exercise1 {
     class Program {
+
         static void Main(string[] args) {
+            //2.1.3
+            var songs = new Song[]  {
+                new Song("a", "bb", 258),
+                new Song("c", "dd", 186),
+                new Song("e", "ff", 293),
+            };
+            PrintSongs(songs);
+        }
+
+        //2.1.4
+        private static void PrintSongs(Song[] songs) {
+            foreach(var song in songs) {
+                Console.WriteLine("タイトル{0},歌手名{1},演奏時間{2:m\\:ss}", song.Title, song.ArtistName, TimeSpan.FromSeconds(song.Length));
+            }
         }
     }
 }
--- Exercise1/Song.cs
+++ Exercise1/Song.cs
@@ -6,6 +6,19 @@ using System.Threading.Tasks;
 
 namespace Exercise1 {
     class Song {
-        
+        //2.1.1
+        //タイトル
+        public string Title { get; set; }
+        //アーティスト名
+        public string ArtistName { get; set; }
+        //演奏時間　単位：秒
+        public int Length { get; set; }
+
+        //2.1.2
+        public Song(string title, string artistName, int length) {
+            Title = title;
+            ArtistName = artistName;
+            Length = length;
+        }
     }
 }
