cfd82d6cd473346c37f80cc7575f6e6ff25e27a3
古里 尚英 <32025@ojs.ac.jp>
2021-05-28 14:12:02 +0900

    P77 リスト2.27_完成版 1.1

--- SalesCalculator/SalesCounter.cs
+++ SalesCalculator/SalesCounter.cs
@@ -18,11 +18,11 @@ namespace SalesCalculator {
         //List 2-15
         //売り上げデータを読み込み、Saleオブジェクトのリストを返す
         private static IEnumerable<Sale> ReadSales(String filePath) {
-            List<Sale> sales = new List<Sale>();
-            string[] lines = File.ReadAllLines(filePath);
-            foreach (string line in lines) {
-                string[] items = line.Split(',');
-                Sale sale = new Sale {
+            var sales = new List<Sale>();
+            var lines = File.ReadAllLines(filePath);
+            foreach (var line in lines) {
+                var items = line.Split(',');
+                var sale = new Sale {
                     ShopName = items[0],
                     ProductCategory = items[1],
                     Amount = int.Parse(items[2])
