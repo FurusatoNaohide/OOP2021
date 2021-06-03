acac4c955bc22588313bce11382b71b877df5e8e
古里 尚英 <32025@ojs.ac.jp>
2021-06-03 15:57:41 +0900

    P99 問題3.1

--- Section01/Program.cs
+++ Section01/Program.cs
@@ -7,21 +7,48 @@ using System.Threading.Tasks;
 namespace Section01 {
     class Program {
         static void Main(string[] args) {
-            var sc = new SampleCode();
-            Console.WriteLine(sc.Count(5));
+            var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
+
+
+            // 3.1.1
+            Exercise1_1(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.2
+            Exercise1_2(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.3
+            Exercise1_3(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.4
+            Exercise1_4(numbers);
+
         }
 
-    }
+        private static void Exercise1_1(List<int> numbers) {
+            var exist = numbers.Exists(s => s % 8 == 0 || s % 9 == 0);
+            if (exist)
+                Console.WriteLine("存在しています。");
+            else
+                Console.WriteLine("存在していません。");
+        }
 
-    class SampleCode {
-        public int Count(int num) {
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
-            int count = 0;
-            foreach (var n in numbers) {
-                if (n == num)
-                    count++;
-            }
-            return count;
+        private static void Exercise1_2(List<int> numbers) {
+            numbers.ForEach(s => Console.WriteLine(s / 2));
+        }
+
+        private static void Exercise1_3(List<int> numbers) {
+            var query = numbers.Where(s => s >= 50);
+            foreach (int i in query)
+                Console.WriteLine(i);
+        }
+
+        private static void Exercise1_4(List<int> numbers) {
+            var query = numbers.Select(s => s * 2);
+            foreach (int i in query)
+                Console.WriteLine(i);
         }
     }
 }
