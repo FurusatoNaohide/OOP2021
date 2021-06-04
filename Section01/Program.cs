02c33196859e5f7bdb9c0dc6b838b76bd9b39583
古里 尚英 <32025@ojs.ac.jp>
2021-06-04 15:56:04 +0900

    P100 問題3.2

--- Section01/Program.cs
+++ Section01/Program.cs
@@ -8,8 +8,7 @@ namespace Section01 {
     class Program {
         static void Main(string[] args) {
             var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
-
-
+            var names = new List<string> { "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong", };
             // 3.1.1
             Exercise1_1(numbers);
             Console.WriteLine("-----");
@@ -24,11 +23,31 @@ namespace Section01 {
 
             // 3.1.4
             Exercise1_4(numbers);
+            Console.WriteLine();
 
+            // 3.2.1
+            Exercise2_1(names);
+            Console.WriteLine();
+
+            // 3.2.2
+            Exercise2_2(names);
+            Console.WriteLine();
+
+            // 3.2.3
+            Exercise2_3(names);
+            Console.WriteLine();
+
+            // 3.2.4
+            Exercise2_4(names);
+            Console.WriteLine();
+
+            //できた人用
+            Exercise2_5(names);
+            Console.WriteLine();
         }
 
         private static void Exercise1_1(List<int> numbers) {
-            var exist = numbers.Exists(s => s % 8 == 0 || s % 9 == 0);
+            var exist = numbers.Exists(n => n % 8 == 0 || n % 9 == 0);
             if (exist)
                 Console.WriteLine("存在しています。");
             else
@@ -36,19 +55,61 @@ namespace Section01 {
         }
 
         private static void Exercise1_2(List<int> numbers) {
-            numbers.ForEach(s => Console.WriteLine(s / 2));
+            numbers.ForEach(n => Console.WriteLine(n / 2.0));
         }
 
         private static void Exercise1_3(List<int> numbers) {
-            var query = numbers.Where(s => s >= 50);
-            foreach (int i in query)
-                Console.WriteLine(i);
+            //メソッドチェーン
+            //numbers.Where(n => n >= 50).ToList().ForEach(Console.WriteLine);
+
+            //var nums = numbers.Where(n => n >= 50);
+            foreach (int n in numbers.Where(n => n >= 50))
+                Console.WriteLine(n);
         }
 
         private static void Exercise1_4(List<int> numbers) {
-            var query = numbers.Select(s => s * 2);
-            foreach (int i in query)
-                Console.WriteLine(i);
+            //リストに格納 .Tolist();
+            List<int> list = numbers.Select(n => n * 2).ToList();
+            foreach (int num in list)
+                Console.WriteLine(num);
+        }
+
+
+        private static void Exercise2_1(List<string> names) {
+            Console.Write("都市名を入力＞");
+            var line = Console.ReadLine();//入力データ取り込み
+            var index = names.FindIndex(s => s == line);
+            Console.WriteLine(index);
+        }
+
+
+        private static void Exercise2_2(List<string> names) {
+            var count = names.Count(s => s.Contains('o'));
+            Console.WriteLine(count);
+        }
+
+
+        private static void Exercise2_3(List<string> names) {
+            //配列に格納 .ToArray();
+            var citys = names.Where(s => s.Contains('o')).ToArray();
+            foreach(var n in citys)           
+                Console.WriteLine(n);
+        }
+
+
+        private static void Exercise2_4(List<string> names) {
+            var count = names.Where(s => s.StartsWith("B")).Select(s => s.Length);
+            foreach (var n in count)
+                Console.WriteLine(n);
+        }
+
+        private static void Exercise2_5(List<string> names) {
+            int count = 0;
+            foreach (var name in names) {
+                //IsLower() 小文字か判別
+                count += name.Count(c => char.IsLower(c));
+            }
+            Console.WriteLine(count);
         }
     }
 }
