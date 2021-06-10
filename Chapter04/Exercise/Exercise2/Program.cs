e868169cbb1f81c37bf67f16a6efaa6e25aad1f6
古里 尚英 <32025@ojs.ac.jp>
2021-06-10 15:59:56 +0900

    P134 問題4.2まで

---
+++ Chapter04/Exercise/Exercise2/Program.cs
@@ -0,0 +1,46 @@
+﻿using Exercise1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2 {
+    class Program {
+        static void Main(string[] args) {
+            //4.2.1
+            var ymCollection = new YearMonth[] {
+                new YearMonth(1980,1), new YearMonth(1990,4), new YearMonth(2000,7),
+                new YearMonth(2010,9), new YearMonth(2020,12),};
+            //4.2.2
+            Exercise2_2(ymCollection);
+            Console.WriteLine("-----");
+            //4.2.4
+            Exercise2_4(ymCollection);
+            Console.WriteLine("-----");
+            //4.2.5
+            Exercise2_5(ymCollection);
+        }
+
+        private static void Exercise2_2(YearMonth[] ymCollection) {
+            foreach (var ym in ymCollection) {
+                Console.WriteLine(ym);
+            }
+        }
+        //4.2.3
+        /*static YearMonth FindFirst21C(YearMonth[] yms) {
+            foreach (var ym in yms) {
+
+            }
+        }*/
+
+        private static void Exercise2_4(YearMonth[] ymCollection) {
+            
+        }
+
+        private static void Exercise2_5(YearMonth[] ymCollection) {
+            
+        }
+
+    }
+}
--- Chapter04/Section01/Program.cs
+++ Chapter04/Section01/Program.cs
@@ -7,13 +7,15 @@ using System.Threading.Tasks;
 namespace Section01 {
     class Program {
         static void Main(string[] args) {
-            /*var person = new Person {
+            /*リスト4.8
+                var person = new Person {
                 Name = "新井遥菜",
                 Birthday = new DateTime(1995, 11, 23),
                 PhoneNumber = "012-3456-7890",
             };*/
 
 
+            //リスト 4.25
             /*var list = new List<int> { 10, 20, 30, 40, };
             var key = 50;
             //条件演算子・参考演算子（? 1 : 0）1←true 0←false
@@ -21,18 +23,45 @@ namespace Section01 {
             Console.WriteLine(num);*/
 
 
+            //リスト 4.27
             /*string code = "12345";
             //null合体演算子（??）
             var message = GetMessage(code) ?? DefaultMessage();
             Console.WriteLine(message);*/
 
 
+            //リスト4.28
             //var ret = GetProductA();
 
 
-            int count = 0;
+            /*int count = 0;
             Console.WriteLine($"後置：{count++}");
-            Console.WriteLine($"前置：{++count}");
+            Console.WriteLine($"前置：{++count}");*/
+
+
+            //リスト4.43
+            /*var str = "12a3";
+
+            //int height;
+            if (int.TryParse(str, out var height)) {
+                Console.WriteLine(height);
+            } else {
+                Console.WriteLine("変換できません");
+            }*/
+
+
+            //リスト 4.44
+            var Session = new Dictionary<string, object>();
+            Session["MyProduct"] = new Product();
+
+            var product = Session["MyProduct"] as Product;
+            if(product.Name == "") {
+                //productが取得できなかった時の処理
+                Console.WriteLine("productが取得できなかった");
+            } else {
+                //productが取得できた時の処理
+                Console.WriteLine("productが取得できた");
+            }
         }
 
         //スタブ
