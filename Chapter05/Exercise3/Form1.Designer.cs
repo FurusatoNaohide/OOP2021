169d26bc200259c793d6b4ee4214ba001c80718d
古里 尚英 <32025@ojs.ac.jp>
2021-06-24 16:00:36 +0900

    P159問題5.3まで

---
+++ Chapter05/Exercise3/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿
+namespace Exercise3 {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputStrText = new System.Windows.Forms.TextBox();
+            this.Button5_3_1 = new System.Windows.Forms.Button();
+            this.TextBoxSpaceCount = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TextBoxWordChange = new System.Windows.Forms.TextBox();
+            this.Button5_3_2 = new System.Windows.Forms.Button();
+            this.Button5_3_3 = new System.Windows.Forms.Button();
+            this.TextBoxWordCount = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Button5_3_4 = new System.Windows.Forms.Button();
+            this.TextBoxReWord = new System.Windows.Forms.TextBox();
+            this.Button5_3_5 = new System.Windows.Forms.Button();
+            this.TextBoxWordCount4 = new System.Windows.Forms.TextBox();
+            this.TextBoxBeforWord = new System.Windows.Forms.TextBox();
+            this.TextBoxAfterWord = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Button5_3_6 = new System.Windows.Forms.Button();
+            this.TextBoxWordChange2 = new System.Windows.Forms.TextBox();
+            this.TextBoxNovel = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "問題5.3";
+            // 
+            // inputStrText
+            // 
+            this.inputStrText.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.inputStrText.Location = new System.Drawing.Point(17, 49);
+            this.inputStrText.Name = "inputStrText";
+            this.inputStrText.Size = new System.Drawing.Size(467, 28);
+            this.inputStrText.TabIndex = 1;
+            // 
+            // Button5_3_1
+            // 
+            this.Button5_3_1.Location = new System.Drawing.Point(17, 106);
+            this.Button5_3_1.Name = "Button5_3_1";
+            this.Button5_3_1.Size = new System.Drawing.Size(94, 41);
+            this.Button5_3_1.TabIndex = 2;
+            this.Button5_3_1.Text = "問題5.3.1";
+            this.Button5_3_1.UseVisualStyleBackColor = true;
+            this.Button5_3_1.Click += new System.EventHandler(this.Button5_3_1_Click);
+            // 
+            // TextBoxSpaceCount
+            // 
+            this.TextBoxSpaceCount.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.TextBoxSpaceCount.Location = new System.Drawing.Point(198, 113);
+            this.TextBoxSpaceCount.Name = "TextBoxSpaceCount";
+            this.TextBoxSpaceCount.Size = new System.Drawing.Size(100, 23);
+            this.TextBoxSpaceCount.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(117, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "空白文字数は";
+            // 
+            // TextBoxWordChange
+            // 
+            this.TextBoxWordChange.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.TextBoxWordChange.Location = new System.Drawing.Point(198, 174);
+            this.TextBoxWordChange.Name = "TextBoxWordChange";
+            this.TextBoxWordChange.Size = new System.Drawing.Size(368, 23);
+            this.TextBoxWordChange.TabIndex = 3;
+            // 
+            // Button5_3_2
+            // 
+            this.Button5_3_2.Location = new System.Drawing.Point(17, 167);
+            this.Button5_3_2.Name = "Button5_3_2";
+            this.Button5_3_2.Size = new System.Drawing.Size(94, 41);
+            this.Button5_3_2.TabIndex = 5;
+            this.Button5_3_2.Text = "問題5.3.2";
+            this.Button5_3_2.UseVisualStyleBackColor = true;
+            this.Button5_3_2.Click += new System.EventHandler(this.Button5_3_2_Click);
+            // 
+            // Button5_3_3
+            // 
+            this.Button5_3_3.Location = new System.Drawing.Point(17, 232);
+            this.Button5_3_3.Name = "Button5_3_3";
+            this.Button5_3_3.Size = new System.Drawing.Size(94, 41);
+            this.Button5_3_3.TabIndex = 6;
+            this.Button5_3_3.Text = "問題5.3.3";
+            this.Button5_3_3.UseVisualStyleBackColor = true;
+            this.Button5_3_3.Click += new System.EventHandler(this.Button5_3_3_Click);
+            // 
+            // TextBoxWordCount
+            // 
+            this.TextBoxWordCount.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.TextBoxWordCount.Location = new System.Drawing.Point(198, 239);
+            this.TextBoxWordCount.Name = "TextBoxWordCount";
+            this.TextBoxWordCount.Size = new System.Drawing.Size(100, 23);
+            this.TextBoxWordCount.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(117, 246);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "単語数は";
+            // 
+            // Button5_3_4
+            // 
+            this.Button5_3_4.Location = new System.Drawing.Point(17, 292);
+            this.Button5_3_4.Name = "Button5_3_4";
+            this.Button5_3_4.Size = new System.Drawing.Size(94, 41);
+            this.Button5_3_4.TabIndex = 7;
+            this.Button5_3_4.Text = "問題5.3.4";
+            this.Button5_3_4.UseVisualStyleBackColor = true;
+            this.Button5_3_4.Click += new System.EventHandler(this.Button5_3_4_Click);
+            // 
+            // TextBoxReWord
+            // 
+            this.TextBoxReWord.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.TextBoxReWord.Location = new System.Drawing.Point(198, 362);
+            this.TextBoxReWord.Name = "TextBoxReWord";
+            this.TextBoxReWord.Size = new System.Drawing.Size(368, 23);
+            this.TextBoxReWord.TabIndex = 3;
+            // 
+            // Button5_3_5
+            // 
+            this.Button5_3_5.Location = new System.Drawing.Point(17, 355);
+            this.Button5_3_5.Name = "Button5_3_5";
+            this.Button5_3_5.Size = new System.Drawing.Size(94, 41);
+            this.Button5_3_5.TabIndex = 8;
+            this.Button5_3_5.Text = "問題5.3.5";
+            this.Button5_3_5.UseVisualStyleBackColor = true;
+            this.Button5_3_5.Click += new System.EventHandler(this.Button5_3_5_Click);
+            // 
+            // TextBoxWordCount4
+            // 
+            this.TextBoxWordCount4.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.TextBoxWordCount4.Location = new System.Drawing.Point(198, 299);
+            this.TextBoxWordCount4.Name = "TextBoxWordCount4";
+            this.TextBoxWordCount4.Size = new System.Drawing.Size(368, 23);
+            this.TextBoxWordCount4.TabIndex = 3;
+            // 
+            // TextBoxBeforWord
+            // 
+            this.TextBoxBeforWord.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.TextBoxBeforWord.Location = new System.Drawing.Point(92, 419);
+            this.TextBoxBeforWord.Name = "TextBoxBeforWord";
+            this.TextBoxBeforWord.Size = new System.Drawing.Size(100, 23);
+            this.TextBoxBeforWord.TabIndex = 3;
+            // 
+            // TextBoxAfterWord
+            // 
+            this.TextBoxAfterWord.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.TextBoxAfterWord.Location = new System.Drawing.Point(92, 448);
+            this.TextBoxAfterWord.Name = "TextBoxAfterWord";
+            this.TextBoxAfterWord.Size = new System.Drawing.Size(100, 23);
+            this.TextBoxAfterWord.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 426);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "置換前";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(35, 455);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "置換後";
+            // 
+            // Button5_3_6
+            // 
+            this.Button5_3_6.Location = new System.Drawing.Point(204, 426);
+            this.Button5_3_6.Name = "Button5_3_6";
+            this.Button5_3_6.Size = new System.Drawing.Size(94, 41);
+            this.Button5_3_6.TabIndex = 9;
+            this.Button5_3_6.Text = "置換";
+            this.Button5_3_6.UseVisualStyleBackColor = true;
+            this.Button5_3_6.Click += new System.EventHandler(this.Button5_3_6_Click);
+            // 
+            // TextBoxWordChange2
+            // 
+            this.TextBoxWordChange2.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.TextBoxWordChange2.Location = new System.Drawing.Point(304, 433);
+            this.TextBoxWordChange2.Name = "TextBoxWordChange2";
+            this.TextBoxWordChange2.Size = new System.Drawing.Size(368, 23);
+            this.TextBoxWordChange2.TabIndex = 3;
+            // 
+            // TextBoxNovel
+            // 
+            this.TextBoxNovel.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.TextBoxNovel.Location = new System.Drawing.Point(12, 497);
+            this.TextBoxNovel.Name = "TextBoxNovel";
+            this.TextBoxNovel.Size = new System.Drawing.Size(467, 28);
+            this.TextBoxNovel.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 576);
+            this.Controls.Add(this.Button5_3_6);
+            this.Controls.Add(this.Button5_3_5);
+            this.Controls.Add(this.Button5_3_4);
+            this.Controls.Add(this.Button5_3_3);
+            this.Controls.Add(this.Button5_3_2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TextBoxWordCount);
+            this.Controls.Add(this.TextBoxReWord);
+            this.Controls.Add(this.TextBoxWordCount4);
+            this.Controls.Add(this.TextBoxWordChange2);
+            this.Controls.Add(this.TextBoxWordChange);
+            this.Controls.Add(this.TextBoxAfterWord);
+            this.Controls.Add(this.TextBoxBeforWord);
+            this.Controls.Add(this.TextBoxSpaceCount);
+            this.Controls.Add(this.Button5_3_1);
+            this.Controls.Add(this.TextBoxNovel);
+            this.Controls.Add(this.inputStrText);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputStrText;
+        private System.Windows.Forms.Button Button5_3_1;
+        private System.Windows.Forms.TextBox TextBoxSpaceCount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TextBoxWordChange;
+        private System.Windows.Forms.Button Button5_3_2;
+        private System.Windows.Forms.Button Button5_3_3;
+        private System.Windows.Forms.TextBox TextBoxWordCount;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Button5_3_4;
+        private System.Windows.Forms.TextBox TextBoxReWord;
+        private System.Windows.Forms.Button Button5_3_5;
+        private System.Windows.Forms.TextBox TextBoxWordCount4;
+        private System.Windows.Forms.TextBox TextBoxBeforWord;
+        private System.Windows.Forms.TextBox TextBoxAfterWord;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button Button5_3_6;
+        private System.Windows.Forms.TextBox TextBoxWordChange2;
+        private System.Windows.Forms.TextBox TextBoxNovel;
+    }
+}
+
---
+++ Chapter05/Exercise3/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise3 {
+    public partial class Form1 : Form {
+        //コンストラクタ 先に呼ばれる
+        public Form1() {
+            InitializeComponent();
+        }
+
+        //フォームがロードされるタイミングで1回だけ実行される
+        private void Form1_Load(object sender, EventArgs e) {
+            inputStrText.Text = "Jackdaws love my big sphinx of quartz";
+            TextBoxNovel.Text = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+        }
+
+        //空白の個数
+        private void Button5_3_1_Click(object sender, EventArgs e) {
+            var count = inputStrText.Text.Count(c => c == ' ');
+            TextBoxSpaceCount.Text = count.ToString();
+        }
+
+        //特定の文字列の置き換え
+        private void Button5_3_2_Click(object sender, EventArgs e) {
+            var replaced = inputStrText.Text.Replace("big", "small");
+            TextBoxWordChange.Text = replaced;
+        }
+
+        //単語の数 Split()で分けた配列の個数をカウント
+        private void Button5_3_3_Click(object sender, EventArgs e) {
+            var counts = inputStrText.Text.Split(' ').Count();
+            TextBoxWordCount.Text = counts.ToString();
+        }
+
+        //4文字以下の単語をつなげて表示
+        private void Button5_3_4_Click(object sender, EventArgs e) {
+            var words = inputStrText.Text.Split(' ').Where(s => s.Length <= 4);
+            foreach (var word in words) {
+                TextBoxWordCount4.Text += word + ' ';
+            };
+        }
+
+        //空白で区切り、配列格納した後、再度文字列連結(StringBuilderクラス)
+        private void Button5_3_5_Click(object sender, EventArgs e) {
+            var sb = new StringBuilder();
+            var words = inputStrText.Text.Split(' ').ToArray();
+            foreach (var word in words) {
+                sb.Append(word + ' ');
+            }
+            TextBoxReWord.Text = sb.ToString();
+        }
+
+        //指定した特定の文字列を指定した別の文字列に置き換え
+        private void Button5_3_6_Click(object sender, EventArgs e) {
+            TextBoxWordChange2.Text = inputStrText.Text.Replace(TextBoxBeforWord.Text, TextBoxAfterWord.Text);
+        }
+    }
+}
