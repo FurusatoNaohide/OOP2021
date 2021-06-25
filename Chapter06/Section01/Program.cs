785becec22e6f7dafafa60569730da3951e24e97
古里 尚英 <32025@ojs.ac.jp>
2021-06-25 15:59:59 +0900

    P164

--- Chapter06/Section01/Program.cs
+++ Chapter06/Section01/Program.cs
@@ -12,7 +12,7 @@ namespace Section01 {
             Console.WriteLine("本の総ページ数は" + books.Sum(x => x.Pages).ToString("#,0") + "ページ");
             Console.WriteLine("最も価格が高い本は" + books.Max(x => x.Price).ToString("#,0") + "円");
             Console.WriteLine("最も価格が安い本は" + books.Min(x => x.Price).ToString("#,0") + "円");
-            //500円以上の本は何冊
+            //500円以上の本は何冊 ※Count(条件式)
             Console.WriteLine("500円以上の本は" + books.Count(x => 500 <= x.Price).ToString("#,0") + "冊");
         }
     }
