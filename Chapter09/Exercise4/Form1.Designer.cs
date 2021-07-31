fe469f882c666f6d538eeceecbf4d1a158ecb772
尚英 古里 <32025@ojs.ac.jp>
2021-07-31 16:38:40 +0900

    問題9.4まで

--- Chapter09/Exercise4/Form1.Designer.cs
+++ Chapter09/Exercise4/Form1.Designer.cs
@@ -26,24 +26,52 @@ namespace Exercise4 {
         private void InitializeComponent() {
             this.fbdFindFolder = new System.Windows.Forms.FolderBrowserDialog();
             this.btChoiseDirectory = new System.Windows.Forms.Button();
+            this.btCopyDirectory = new System.Windows.Forms.Button();
+            this.btCopyFile = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btChoiseDirectory
             // 
-            this.btChoiseDirectory.Location = new System.Drawing.Point(12, 12);
+            this.btChoiseDirectory.Location = new System.Drawing.Point(16, 15);
+            this.btChoiseDirectory.Margin = new System.Windows.Forms.Padding(4);
             this.btChoiseDirectory.Name = "btChoiseDirectory";
-            this.btChoiseDirectory.Size = new System.Drawing.Size(96, 31);
+            this.btChoiseDirectory.Size = new System.Drawing.Size(128, 39);
             this.btChoiseDirectory.TabIndex = 0;
             this.btChoiseDirectory.Text = "指定ディレクトリ";
             this.btChoiseDirectory.UseVisualStyleBackColor = true;
             this.btChoiseDirectory.Click += new System.EventHandler(this.btChoiseDirectory_Click);
             // 
+            // btCopyDirectory
+            // 
+            this.btCopyDirectory.Location = new System.Drawing.Point(16, 62);
+            this.btCopyDirectory.Margin = new System.Windows.Forms.Padding(4);
+            this.btCopyDirectory.Name = "btCopyDirectory";
+            this.btCopyDirectory.Size = new System.Drawing.Size(128, 39);
+            this.btCopyDirectory.TabIndex = 1;
+            this.btCopyDirectory.Text = "複製先ディレクトリ";
+            this.btCopyDirectory.UseVisualStyleBackColor = true;
+            this.btCopyDirectory.Click += new System.EventHandler(this.btCopyDirectory_Click);
+            // 
+            // btCopyFile
+            // 
+            this.btCopyFile.Location = new System.Drawing.Point(16, 109);
+            this.btCopyFile.Margin = new System.Windows.Forms.Padding(4);
+            this.btCopyFile.Name = "btCopyFile";
+            this.btCopyFile.Size = new System.Drawing.Size(128, 39);
+            this.btCopyFile.TabIndex = 2;
+            this.btCopyFile.Text = "コピー";
+            this.btCopyFile.UseVisualStyleBackColor = true;
+            this.btCopyFile.Click += new System.EventHandler(this.btCopyFile_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(120, 116);
+            this.ClientSize = new System.Drawing.Size(159, 151);
+            this.Controls.Add(this.btCopyFile);
+            this.Controls.Add(this.btCopyDirectory);
             this.Controls.Add(this.btChoiseDirectory);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -54,6 +82,8 @@ namespace Exercise4 {
 
         private System.Windows.Forms.FolderBrowserDialog fbdFindFolder;
         private System.Windows.Forms.Button btChoiseDirectory;
+        private System.Windows.Forms.Button btCopyDirectory;
+        private System.Windows.Forms.Button btCopyFile;
     }
 }
 
--- Chapter09/Exercise4/Form1.cs
+++ Chapter09/Exercise4/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,12 +11,34 @@ using System.Windows.Forms;
 
 namespace Exercise4 {
     public partial class Form1 : Form {
+        string selectDirectory = ""; //指定ディレクトリ
+        string copyDirectory = "";//コピー先ディレクトリ
+
         public Form1() {
             InitializeComponent();
         }
 
         private void btChoiseDirectory_Click(object sender, EventArgs e) {
+            if (fbdFindFolder.ShowDialog() == DialogResult.OK){
+                selectDirectory = fbdFindFolder.SelectedPath;
+            }
+        }
+
+        private void btCopyDirectory_Click(object sender, EventArgs e){
+            if(fbdFindFolder.ShowDialog() == DialogResult.OK){
+                copyDirectory = fbdFindFolder.SelectedPath;
+            }
+        }
+
+        private void btCopyFile_Click(object sender, EventArgs e){
+            var di = new DirectoryInfo(selectDirectory);
             
+            var files = di.EnumerateFiles("*.txt", SearchOption.AllDirectories);
+            foreach (var file in files){
+                var fwe = Path.GetFileNameWithoutExtension(file.FullName);
+                var extension = Path.GetExtension(file.Extension);
+                File.Copy(file.FullName, copyDirectory + '\\' + fwe + "_bak" + extension, overwrite:true);
+            }
         }
     }
 }
