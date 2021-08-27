2b5b9971be642c02997f41bff4b9f79aababa480
尚英 古里 <32025@ojs.ac.jp>
2021-08-27 14:54:11 +0900

    問題11_1_1

---
+++ Chapter11/Exercise1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = "Sample.xml";
+            Exercise1_1(file);
+            Console.WriteLine("-------");
+
+            Exercise1_2(file);
+            Console.WriteLine("-------");
+
+            Exercise1_3(file);
+            Console.WriteLine("-------");
+            
+        }
+
+        private static void Exercise1_1(string file)
+        {
+            var xdoc = XDocument.Load(file);
+            var xelements = xdoc.Root.Elements();
+            foreach (var xnovelist in xelements)
+            {
+                var xname = xnovelist.Element("name");
+                var members = xnovelist.Element("teammembers");
+                Console.WriteLine("{0} {1}", xname.Value, int.Parse(members.Value));
+            }
+
+        }
+
+        private static void Exercise1_2(string file)
+        {
+            
+        }
+
+        private static void Exercise1_3(string file)
+        {
+            var xdoc = XDocument.Load(file);
+            var xelements = xdoc.Root.Elements();
+        }
+    }
+}
