c4bd353f38be9cf1f15b32866b885718b0f75d48
尚英 古里 <32025@ojs.ac.jp>
2021-09-02 13:28:03 +0900

    問題11_2 完成

--- Chapter11/Exercise1/Program.cs
+++ Chapter11/Exercise1/Program.cs
@@ -117,37 +117,37 @@ namespace Exercise1
         {
             var newfile = "sports.xml";//出力する新しいファイル
             //P290 リスト11.5を参考
+            //追加するデータ
             var element = new XElement("ballsport",
                                 new XElement("name","サッカー",new XAttribute("kanji","蹴球")),
                                 new XElement("temmembers","11"),
                                 new XElement("firstplayed","1863")
                                 );
-            var xdoc = XDocument.Load(file);
-            xdoc.Root.Add(element);
+            var xdoc = XDocument.Load(file);//追加先のXMLファイルを読み込む
+            xdoc.Root.Add(element);//追加先へ追加
             xdoc.Save(newfile);
             
         }
 
         private static void Exercise2(string file2)
         {
-            /*未完成
+            /*完成*/
             var newfile = "11_2_bak.xml";//出力する新しいファイル
+            //リスト11.27を参考 XMLファイルから
             var xdoc = XDocument.Load(file2);
-            var pairs = xdoc.Root.Elements()
+            var dict = xdoc.Root.Elements()
                             .Select(x => new
                             {
-                                Key = x.Element("kanji").Value,
-                                Value = x.Element("yomi").Value
+                                Kanji = x.Element("kanji").Value,
+                                Yomi = x.Element("yomi").Value
                             });
-            var dict = pairs.ToDictionary(x => x.Key, x => x.Value);
-            var word = new XElement("word");
-            foreach (var d in dict)
-            {
-                word.SetAttributeValue(d.Key,d.Value);
-            }
-            var root = new XElement("difficultkanji", word);
+            //リスト11.26を参考 新しいXMLファイルを作成
+            var query = dict.Select(x => new XElement("word",
+                                            new XAttribute("kanji", x.Kanji),
+                                            new XAttribute("yomi", x.Yomi)));
+            var root = new XElement("difficultkanji", query);
             root.Save(newfile);
-            */
+            
         }
 
     }
