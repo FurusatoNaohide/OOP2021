c6303046e4180be464dd4efae80de983e3eead83
尚英 古里 <32025@ojs.ac.jp>
2021-11-17 16:00:12 +0900

    NumberGame 可変用プログラム解説付き

--- WPF/NumberGame/MainWindow.xaml.cs
+++ WPF/NumberGame/MainWindow.xaml.cs
@@ -23,11 +23,12 @@ namespace NumberGame
         int Cnum;
         int Rnum;
         int Count = 5;
+        private const int Rows = 5;//行
+        private const int Colums = 5;//列
+        Random randoms = new Random();
         public MainWindow()
         {
             InitializeComponent();
-            Rnum = new Random().Next(1, 25);
-            TbMessage.Text = string.Format("選ばれた数字を{0}回以内で当ててください",Count);
         }
         private void Number_Click(object sender, RoutedEventArgs e)
         {
@@ -61,5 +62,53 @@ namespace NumberGame
                 Close();
             }
         }
+        //ロード時に一度だけ実行される
+        private void MainDisp_Loaded(object sender, RoutedEventArgs e)
+        {
+            //List<Button> buttons = new List<Button>();
+            //正解を取得
+            Rnum = new Random().Next(1, 25);
+            TbMessage.Text = string.Format("選ばれた数字を{0}回以内で当ててください", Count);
+
+            /*可変
+            Rnum = randoms.Next(Rows, Colums) + 1;
+
+            //行
+            for (int i = 0; i < Rows; i++)
+            {
+                grid.RowDefinitions.Add(new RowDefinition());
+            }
+            //列
+            for (int j = 0; j < Colums; j++)
+            {
+                grid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Colums; j++)
+                {
+                    var bt = new Button();
+                    bt.Width = MainForm.Width / Colums;
+                    bt.Height = MainForm.Height / Rows;
+                    bt.Content = (j + 1)+(i * 5);
+                    bt.FontSize = 20;
+                    bt.Click += Bt_Click;
+                    Grid.SetRow(bt, 1);
+                    Grid.SetColumn(bt, 1);
+                    buttons.Add(bt);
+                }
+            }
+            buttons.ForEach(bt => grid.Children.Add(bt));
+            MainForm.Height += textDisp.Height + 50;
+            */
+        }
+            
+        private void Bt_Click(object sender, RoutedEventArgs e)
+        {
+            Button selectedButton = (Button)sender;
+            int num = (int)selectedButton.Content;
+        }
+        
     }
 }
